78ac9ddd5e2157bedb5499d0734a03af2d866c6a
Anbuselvan Rocky <15264938+anburocky3@users.noreply.github.com>
2024-02-25 11:37:26 +0530

    client: init

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# Tutorial Request - Client App
---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      Hello Client here!
+      {/* 1. List existing request made by the people */}
+      {/* 2. Create the tutorial request */}
+      {/* 3. I should change/delete my tutorial request which i have created earlier. */}
+    </div>
+  );
+};
+
+export default App;
